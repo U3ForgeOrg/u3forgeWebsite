c46c235362040d875e43518cf39a31e85d841a59
AbdElRahman Yousry <ar.yousry@gmail.com>
2023-04-29 09:29:41 +0300

    Test Platform revert

--- Scenes/Char.gd
+++ Scenes/Char.gd
@@ -3,8 +3,7 @@ extends AnimatedSprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if OS.get_name() == "HTML5":
-		get_parent().get_node("Control").get_node("Label").text=OS.get_model_name()
+	pass
 		
 
 
