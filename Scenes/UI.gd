17439354b724c08e3569ac2d34e5781dc30b9746
AbdElRahman Yousry <ar.yousry@gmail.com>
2023-04-30 04:59:12 +0300

    Test platform5

--- Scenes/UI.gd
+++ Scenes/UI.gd
@@ -8,15 +8,26 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# Retrieve the `window.console` object.
-	var console = JavaScript.eval("navigator.appVersion;")
+	var path="res://Scripts/detect_os.txt"
+	var jscode=load_text_file(path)
+	print(jscode)
+	var console = JavaScript.eval(jscode)
 	if console == null:
 		get_node("Label").text="null"
 	else:
 		get_node("Label").text=console
-		# Call the `window.console.log()` method.
-		
 
+
+		
+func load_text_file(path):
+	var f = File.new()
+	var err = f.open(path, File.READ)
+	if err != OK:
+		printerr("Could not open file, error code ", err)
+		return ""
+	var text = f.get_as_text()
+	f.close()
+	return text
 
 
 
